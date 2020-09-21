bc27fed479ab59619ebcef6e9fbf992d573c571d
Ashley Gillenwater <ashleygillenwater@Ashleys-Air.nc.rr.com>
2020-09-21 12:02:08 -0400

    skeleton app structure, added styled-components

---
+++ src/Components/MovieCard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function MovieCard () {
+  return (
+    <h1>MovieCard</h1>
+    // res.results, title: results.title, img: baseURL+ results.poster_path, rating: vote_average, id: results.id 
+  )
+}
+
+export default MovieCard
---
+++ src/Components/MovieResults.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+function MovieResults () {
+  return (
+    <>
+      <h1>Movie Results</h1>
+      //loop over results and generate movie card here
+      <MovieCard />
+    </>
+  )
+}
+
+export default MovieResults
