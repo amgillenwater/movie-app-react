96b621cbb38ce1fa9ea0e816c931819108a8474c
Ashley Gillenwater <amgillenwater@gmail.com>
2020-09-22 11:25:13 -0400

    deploy fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
-import Header from './components/Header.js'
-import SearchBar from './components/SearchBar.js'
+import Header from './Components/Header.js'
+import SearchBar from './Components/SearchBar.js'
 
 function App () {
   return (
